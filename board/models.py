b6a65d9367169974ee6216cfbda7b800d68d689a
Ansesteri <angrylemon5678@gmail.com>
2023-10-05 18:37:38 +0300

    Add admin, Meta

--- board/models.py
+++ board/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here.
 class BoardMessage(models.Model):
    title = models.CharField(max_length=50)
-   content = models.TextField(null=True, blank=True)
+   content = models.TextField(null=True, blank=True, verbose_name="Description")
    price = models.FloatField(null=True, blank=True)
-   published = models.DateTimeField(auto_now_add=True, db_index=True)
+   published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name="Date")
+
+   class Meta:
+      verbose_name="Board of announcements"
+      verbose_name_plural="Board of announcements"
+      ordering = ['-published']
--- board/views.py
+++ board/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import BoardMessage
 
 def index(request):
-   posts = BoardMessage.objects.order_by('-published')
+   posts = BoardMessage.objects.all()
    context = {'posts': posts}
    return render(request, 'board/index.html', context)
