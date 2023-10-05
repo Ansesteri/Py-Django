8efaac5c980efae19af2b7dc352df0e220ae7408
Ansesteri <angrylemon5678@gmail.com>
2023-10-05 18:45:15 +0300

    Add boardmessageadmin

--- board/admin.py
+++ board/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import BoardMessage
 
-admin.site.register(BoardMessage)
+class BoardMessageAdmin(admin.ModelAdmin):
+   list_display = ('title', 'content', 'price', 'published')
+   list_display_links = ('title', 'content')
+   search_fields = ('title', 'content')
+
+admin.site.register(BoardMessage, BoardMessageAdmin)
